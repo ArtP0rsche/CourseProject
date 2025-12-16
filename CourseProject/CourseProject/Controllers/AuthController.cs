1373283e2aefd74f91a64591f2fa1901291ba7f6
ArtP0rsche <ArtP0rsche@yandex.ru>
2025-12-16 09:07:52 +0300

    Update project

---
+++ CourseProject/CourseProject/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using CourseProject.Data;
+using CourseProject.DataModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProject.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly EmploymentServiceContext _context;
+
+        public AuthController(EmploymentServiceContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind("Username,Password")] User @user)
+        {
+            var currentUser = _context.Users.FirstOrDefault(u => u.Username == @user.Username);
+
+            if (currentUser != null && currentUser.Password == user.Password)
+            {
+                HttpContext.Session.SetString("UserRole", _context.Roles.Where(r => r.RoleId == currentUser.RoleId).Select(r => r.Name).FirstOrDefault());
+                HttpContext.Session.SetString("UserFullname", $"{currentUser.Surname} {currentUser.Name} {currentUser.Patronymic}");
+                HttpContext.Session.SetString("UserId", currentUser.UserId.ToString());
+
+                return RedirectToAction("Index", "Events");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Неверный логин или пароль");
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Events");
+        }
+    }
+}
---
+++ CourseProject/CourseProject/Controllers/RequestsController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CourseProject.Data;
+using CourseProject.DataModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CourseProject.Controllers
+{
+    public class RequestsController : Controller
+    {
+        private readonly EmploymentServiceContext _context;
+
+        public RequestsController(EmploymentServiceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Requests
+        public async Task<IActionResult> Index()
+        {
+            var employmentServiceContext = _context.Requests.Include(r => r.Event).Include(r => r.User);
+            ViewBag.StatusOptions = new List<string> { "На рассмотрении", "Отклонена", "Принята" };
+            return View(await employmentServiceContext.ToListAsync());
+        }
+
+        // GET: Requests/Create
+        public IActionResult Create(int? eventId, int? userId)
+        {
+            if (eventId == null || userId == null)
+            {
+                return BadRequest();
+            }
+
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Requests/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int eventId, int userId, [Bind("RequestId,UserId,EventId,Content,Status,UpdatedOn,Institution,PeopleNumber")] Request request)
+        {
+            if (request.EventId != null && request.UserId != null)
+            {
+                request.UpdatedOn = DateOnly.FromDateTime(DateTime.Now);
+                _context.Add(request);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Events");
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", request.EventId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", request.UserId);
+            return View();
+        }
+
+        [HttpPost, ActionName("UpdateStatus")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateStatus(int id, string status)
+        {
+            var request = _context.Requests.FirstOrDefault(r => r.RequestId == id);
+
+            request.Status = status;
+
+            _context.Update(request);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("index", "Requests");
+        }
+
+        // GET: Requests/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Requests.FindAsync(id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return View(request);
+        }
+
+        // POST: Requests/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RequestId,UserId,EventId,Content,Status,UpdatedOn,Institution,PeopleNumber")] Request request)
+        {
+            if (id != request.RequestId)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                request.UpdatedOn = DateOnly.FromDateTime(DateTime.Now);
+                _context.Update(request);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RequestExists(request.RequestId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("Edit", "Users", new { id = Convert.ToInt32(HttpContext.Session.GetString("UserId")) });
+        }
+
+        // POST: Requests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var request = await _context.Requests.FindAsync(id);
+            if (request != null)
+            {
+                _context.Requests.Remove(request);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Edit", "Users", new { id = Convert.ToInt32(HttpContext.Session.GetString("UserId")) });
+        }
+
+        private bool RequestExists(int id)
+        {
+            return _context.Requests.Any(e => e.RequestId == id);
+        }
+    }
+}
